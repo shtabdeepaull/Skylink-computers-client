217d2d8a3b68cba3cda38d0081070bcb4ab4bb24
shtabdeepaull <shtabdee30@gmail.com>
2022-05-24 10:30:03 +0600

    created fake data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Header from './Pages/Shared/Header';
 
 function App() {
   return (
     <div >
-     
+      <Header></Header>
+      <Routes>
+        <Route></Route>
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,20 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#E0FFFF",
+          secondary: "#87CEEB",
+          accent: "#404040",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      // "dark",
+      // "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
